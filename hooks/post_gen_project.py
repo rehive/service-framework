b916755c569cb888a58f6362b6e9686d084c1d4f
Johan <johandp92@gmail.com>
2018-03-20 14:27:05 +0200

    Added etc/ and src/ dirs for k8s setup

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,7 @@
 import os
 import random
 import string
+import shutil
 
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
@@ -32,25 +33,41 @@ def make_django_secret():
         _file.write(file_data)
 
 
-def remove_travis_files():
-    """Remove associated Travis CI files"""
-    file_names = ['.travis.yml']
-    remove_files(file_names)
+def remove_heroku_travis_files():
+    """
+    Remove associated Heroku and TravisCI files.
+    All k8s setup files are located in etc/
+    """
+    file_names = [
+        '.travis.yml', 'Procfile', 'docker-compose.yml', 'Dockerfile',
+        'postgres_ready.py'
+    ]
+    for file_name in file_names:
+        file_path = os.path.join(PROJECT_DIRECTORY, file_name)
+        if os.path.exists(file_path):
+            os.remove(file_path)
+            assert file_path not in os.listdir(PROJECT_DIRECTORY)
 
 
-def remove_heroku_files():
-    """Remove associated Heroku files"""
-    file_names = ['Procfile']
-    remove_files(file_names)
+def remove_gcould_k8s_files():
+    """
+    Move all files out of src/ to the PROJECT_DIR for heroku and travis 
+    deployment setup.
+    """
+    os.rmdir(os.path.join(PROJECT_DIRECTORY, 'etc'))
+    assert 'src' in os.listdir(PROJECT_DIRECTORY)
+    files = os.listdir(os.path.join(PROJECT_DIRECTORY, 'src'))
+    for f in files:
+        shutil.move(f, PROJECT_DIRECTORY)
+    assert 'manage.py' in os.listdir(PROJECT_DIRECTORY)
+    os.rmdir(os.path.join(PROJECT_DIRECTORY, 'src'))
 
 
 # Create django secret
 make_django_secret()
 
 # Check use travis
-if '{{cookiecutter.use_travis_ci}}' == 'n':
-    remove_travis_files()
-
-# Check use Heroku
-if '{{cookiecutter.use_heroku}}' == 'n':
-    remove_heroku_files()
+if '{{cookiecutter.server_infrastructure}}' == 'Heroku + TravisCI':
+    remove_gcould_k8s_files()
+elif '{{cookiecutter.server_infrastructure}}' == 'GCloud + Kubernetes':
+    remove_heroku_travis_files()
