e02a8c0b441254f0a122327f67c12ba94d7f3df3
Michail Brynard <mbrynard@gmail.com>
2020-05-08 17:35:31 +0200

    Update template to utilise latest Rehive deployment setup

--- {{cookiecutter.github_repository_name}}/src/config/plugins/database.py
+++ {{cookiecutter.github_repository_name}}/src/config/plugins/database.py
@@ -4,7 +4,7 @@ options = {
         'connect_timeout': 25,
     }
 
-if not os.environ.get('DEBUG'):
+if not os.environ.get('POSTGRES_SSL_DISABLE') in [True, "True", 'true']:
     options['sslmode'] = 'require'
 
 DATABASES = {
