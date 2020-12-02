975e2786a6ea9cc0dad64ef11291caa51c2ad3e6
Joshua van Besouw <OdinsPlasmaRifle@users.noreply.github.com>
2020-12-02 16:40:51 +0200

    Updated yasg settings

--- {{cookiecutter.github_repository_name}}/src/config/plugins/yasg.py
+++ {{cookiecutter.github_repository_name}}/src/config/plugins/yasg.py
@@ -1,12 +1,18 @@
 SWAGGER_SETTINGS = {
+    'USE_SESSION_AUTH': False,
     'SECURITY_DEFINITIONS': {
         'Bearer': {
             'type': 'apiKey',
             'name': 'Authorization',
-            'in': 'header',
+            'in': 'header'
         }
     },
-    'USE_SESSION_AUTH': False,
-    'DEFAULT_MODEL_RENDERING': 'example',
-    'SHOW_EXTENSIONS': False,
+    'DOC_EXPANSION': 'none',
+    'DEFAULT_MODEL_RENDERING': 'example'
+}
+
+REDOC_SETTINGS = {
+    'LAZY_RENDERING': True,
+    'NATIVE_SCROLLBARS': True,
+    'REQUIRED_PROPS_FIRST': True
 }
